0dcc4b3765a896fd46df8c8506bc36f43db196f1
esther <hadasakus@gmail.com>
2018-06-28 13:45:22 +0100

    heroku Initial commit

--- finance/finance/wsgi.py
+++ finance/finance/wsgi.py
@@ -9,13 +9,19 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 
 import os
 
+# from django.core.wsgi import get_wsgi_application
+
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "finance.settings")
+
+# application = get_wsgi_application()
+
+# # modified for heroku
+# # from whitenoise.django import DjangoWhiteNoise
+# # application = DjangoWhiteNoise(application)
+
+
+
 from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "finance.settings")
-
-application = get_wsgi_application()
-
-# modified for heroku
-# from whitenoise.django import DjangoWhiteNoise
-# application = DjangoWhiteNoise(application)
-
+application = Cling(get_wsgi_application())
